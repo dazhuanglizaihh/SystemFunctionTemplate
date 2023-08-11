97e7a3bffea2c5c6f919822d1e4571998a8f2a0e
wml <1980352822@qq.com>
2023-08-11 08:49:58 +0800

    删除旧user，创建新user表

--- src/main/resources/malldb.sql
+++ src/main/resources/malldb.sql
@@ -1,13 +1,25 @@
-CREATE TABLE user
-(
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(25),
-    email VARCHAR(25),
-    password VARCHAR(8),
-    status int(4)
-);
+drop table if exists 'user';
+create table 'user'(
+    'id' int(11) not null auto_increment,
+    'name' varchar(20) collate utf8_unicode_ci not null,
+    'email' varchar(20) collate utf8_unicode_ci default '',
+    'password' varchar(16) collate utf8_unicode_ci not null,
+    'createtime' timestamp not null default '',
+) engine=innodb default charset=utf8 collate=utf8_unicode_ci;
 
-#测试数据
-INSERT INTO user
-(id, username, email, password, status)
-VALUES ('wml', '1980352822@qq.com', 'qwer8765', 1);
+
+--drop table user;
+-- CREATE TABLE user
+-- (
+--     id INT(11) AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(25),
+--     email VARCHAR(25),
+--     password VARCHAR(8),
+--     status int(4)
+-- );
+--
+-- #测试数据
+-- INSERT INTO user
+-- (id, username, email, password, status)
+-- VALUES ('wml', '1980352822@qq.com', 'qwer8765', 1);
+
